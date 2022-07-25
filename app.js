50bade6fbf8bba2f73c4c4e3a73ff66c3b0d8380
serra <serra@modemgirl.ca>
2022-07-25 09:37:15 -0700

    testing mapbox

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
 const ejsMate = require('ejs-mate');
+const mapboxgl = require('mapbox-gl'); 
+
+// mapboxgl.accessToken = process.env.MBAC;
+
 
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs'); 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -15,5 +21,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(3000, () => {
-  console.log('feeling kinda trippy.... 🍄');
+  console.log('🎒 hey there tripster... ');
 })
