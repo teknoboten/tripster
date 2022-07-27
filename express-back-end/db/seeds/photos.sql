265ba68f05d22388078d0cc31a9e235dc00cf904
serra <serra@modemgirl.ca>
2022-07-27 11:42:48 -0700

    add tables to db

---
+++ express-back-end/db/seeds/photos.sql
@@ -0,0 +1,1 @@
+INSERT INTO photos (id, photo_text, date, latitude, longitude, trip_id ) VALUES (2, 'omg we are at the airport!', '2018-02-12T08:01:00.000Z',  -123.182952566661, 49.19654541524935, 1 );
---
+++ express-back-end/db/seeds/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, first_name, last_name, email) VALUES (1, 'Fjord', 'Windstar', 'fjordwindstar@gmail.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES (2, 'Captain', 'Tripps', 'captaintripzz@hotmail.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES (3, 'Sofia', 'Valdez', 'futureprez@yahoo.ca');
