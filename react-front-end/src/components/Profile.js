3928b29777fc05f8a0e6b8128ddd4b5b0112320c
amyfranses <amyfranses@gmail.com>
2022-07-28 16:32:31 +0000

    create react component templates

---
+++ react-front-end/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div>
+      {/* profile img */}
+      {/* header img */}
+      {/* user name */}
+    </div>
+  );
+}
