6862cb4096b75ca71bb9e766309546502dc31d20
Jeff Bush <jeffbushdesign@gmail.com>
2022-07-28 14:31:47 -0400

    Edit routes

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -14,33 +14,51 @@ App.use(Express.static('public'));
 // }));
 
 // Homepage
-App.get("/", (req, res) => { console.log("mumbo-jumbo"); });
+App.get("/", (req, res) => {
+  res.send("Homepage");
+});
 
 
 // ----- USERS ----- //
 // Landing page for logged-in users
 // Show all of a user's trips
-App.get("/api/users/:id", (req, res) => { });
+App.get("/api/users/:id", (req, res) => {
+  res.send("Landing page for logged-in users, Show all of a user's trips");
+});
 
 // ----- TRIPS ----- //
-// trip homepage showing map and all photos from a trip 
-App.get("/api/trips/:id", (req, res) => { });
 
 // form to create a new trip
-App.get("/api/trips/new", (req, res) => console.log("form to create a new trip"));
+App.get("/api/trips/new", (req, res) => {
+  res.send("form to create a new trip");
+});
 
 // create a new trip in the db
-App.post("/api/trips/new", (req, res) => { });
+App.post("/api/trips/new", (req, res) => {
+  res.send("create a new trip in the db");
+});
+
+// trip homepage showing map and all photos from a trip 
+App.get("/api/trips/:id", (req, res) => {
+  res.send("trip homepage showing map and all photos from a trip");
+});
 
 // ----- PHOTOS ----- //
-// show page of an individual photo with zoomed in map and description
-App.get("/api/photos/:id", (req, res) => { });
 
 // form to upload more photos
-App.get("/api/photos/new", (req, res) => { });
+App.get("/api/photos/new", (req, res) => {
+  res.send("form to upload more photos");
+});
 
 // create new photos in db
-App.post("/api/photos", (req, res) => { });
+App.post("/api/photos", (req, res) => {
+  res.send("create new photos in db");
+});
+
+// show page of an individual photo with zoomed in map and description
+App.get("/api/photos/:id", (req, res) => {
+  res.send("show page of an individual photo with zoomed in map and description");
+});
 
 
 
