61537d5ad267b98dfcc5a80e1201409469886344
amyfranses <amyfranses@gmail.com>
2022-08-01 21:22:18 +0000

    create modal for enlarged image

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import ReactDOM from 'react-dom';
 // import { Route, Switch } from 'react-router-dom';
 
@@ -13,13 +13,18 @@ import React from "react";
 import UploadImageForm from "./components/images/UploadImageForm";
 import Title from "./components/Title";
 import ImageGrid from "./components/images/imageGrid";
+import Modal from "./components/images/Modal";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <Title />
       <UploadImageForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
