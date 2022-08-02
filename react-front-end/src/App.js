590a342a1a6fe156793cf754270e7860e5cc58b6
Jeff Bush <jeffbushdesign@gmail.com>
2022-08-02 13:43:20 -0400

    Fix app.js and add a NotApp.js

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,33 +1,28 @@
-import React, { useState } from "react";
-// import ReactDOM from 'react-dom';
-// import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Switch } from 'react-router-dom';
 
-// import Layout from './components/layout/Layout';
-// import AllTripsPage from './pages/AllTripsPage';
-// import NewTripPage from './pages/NewTripPage';
+import Layout from './components/layout/Layout';
+import AllTripsPage from './pages/AllTripsPage';
+import NewTripPage from './pages/NewTripPage';
 
 // import React from "react";
 // import axios from "axios";
 // import "./App.css";
 
-import UploadImageForm from "./components/images/UploadImageForm";
-import Title from "./components/Title";
-import ImageGrid from "./components/images/imageGrid";
-import Modal from "./components/images/Modal";
-
 function App() {
-  const [selectedImg, setSelectedImg] = useState(null);
   return (
-    <div className="App">
-      <Title />
-      <UploadImageForm />
-      <ImageGrid setSelectedImg={setSelectedImg} />
-      {selectedImg && (
-        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
-      )}
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllTripsPage />
+        </Route>
+        <Route path='/new-trip'>
+          <NewTripPage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
-// test
-export default App;
 
+export default App;
---
+++ react-front-end/src/NotApp.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+// import ReactDOM from 'react-dom';
+// import { Route, Switch } from 'react-router-dom';
+
+// import Layout from './components/layout/Layout';
+// import AllTripsPage from './pages/AllTripsPage';
+// import NewTripPage from './pages/NewTripPage';
+
+// import React from "react";
+// import axios from "axios";
+// import "./App.css";
+
+import UploadImageForm from "./components/images/UploadImageForm";
+import Title from "./components/Title";
+import ImageGrid from "./components/images/imageGrid";
+import Modal from "./components/images/Modal";
+
+function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+  return (
+    <div className="App">
+      <Title />
+      <UploadImageForm />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
+    </div>
+  );
+}
+// test
+export default App;
+
