3f305e450fd50a0d2f585cb436d50a65508b0072
serra <serra@modemgirl.ca>
2022-08-02 12:30:55 -0700

    remove image grid css from index.css

--- react-front-end/src/components/images/imageGrid.js
+++ react-front-end/src/components/images/imageGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useFirestore from "../../hooks/useFirestore";
+import classes from './ImageGrid.module.css';
 import { motion } from "framer-motion";
 
 const ImageGrid = ({ setSelectedImg }) => {
