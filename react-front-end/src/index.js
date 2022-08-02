8bc112b270db2b192132e78359a693d972dd8238
amyfranses <amyfranses@gmail.com>
2022-08-02 17:30:45 +0100

    move app.js

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -21,7 +21,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./NotApp";
 
 ReactDOM.render(
   <React.StrictMode>
