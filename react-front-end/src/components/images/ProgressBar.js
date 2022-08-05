5fd0d1d7d7a2391537cb256101e9263068fa7e6f
serra <serra@modemgirl.ca>
2022-08-05 16:06:36 -0700

    update img grid source to show seed data

--- react-front-end/src/components/images/ProgressBar.js
+++ react-front-end/src/components/images/ProgressBar.js
@@ -1,15 +1,40 @@
 import React, { useEffect } from "react";
 import useStorage from "../../hooks/useStorage";
+import { useParams } from "react-router-dom";
 import { motion } from "framer-motion";
 import classes from "./ImageGrid.module.css";
 
 const ProgressBar = ({ file, setFile, setStoredUrl }) => {
   const { url, progress } = useStorage(file);
 
+  const params = useParams();
+  const trip_id = params.trip_id;
+
+  async function updateDb(url, trip_id) {
+
+    const newImage = {
+       photo_text: "some text about a picture",
+       date: '2018-02-18T08:01:00.000Z'  ,
+       lat: 11.2793497,
+       long: 43.8274546,
+       photo_url: url,
+       trip_id: trip_id  
+     };
+ 
+    const response = await fetch("/api/photos/new", {
+     method: "POST",
+     body: JSON.stringify(newImage),
+     headers: { "Content-Type": "application/json" }
+     });
+ 
+     console.log("response:", response);
+   }
+ 
   useEffect(() => {
     if (url) {
       setFile(null);
       setStoredUrl(url);
+      updateDb(url, trip_id);
     }
   }, [url, setFile ]);
 
