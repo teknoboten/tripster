cab697fcd22214f4cd502820b265805071226892
amyfranses <amyfranses@gmail.com>
2022-08-07 18:53:12 +0000

    update trip detail page and image upload form so that the image grid renders without refresh

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -72,97 +72,73 @@ App.get("/api/trips", async (req, res) => {
 });
 
 // create a new trip in the db
-App.post("/api/trips/new", (req, res) => {
-  // console.log(req.body);
-  // console.log(req.body.tripName);
+App.post("/api/trips/new", async (req, res) => {
   try {
-    pool.connect(async (error, client, release) => {
-      let resp = await client.query(
-        `
+    const result = await db.query(
+      `
         INSERT INTO trips (trip_name)
         VALUES($1) 
         RETURNING id;
       `,
-        [req.body.tripName]
-      );
-      res.json(resp.rows[0]);
-      // res.send(`A new trip to ${req.body.tripName} has been created.`);
-      // needs the thing to prevent SQL injection
-
-      // don't redirect
-      // res.json with what it got
-      // res.redirect('/api/users/:id');
-    });
+      [req.body.tripName]
+    );
+    console.log(result.rows[0]);
+    res.json(result.rows[0]);
   } catch (error) {
-    console.log(error);
+    res.send({ error: error });
   }
 });
 
 // trip detail page showing map and all photos from a trip
-App.get("/api/trips/:id", (req, res) => {
+App.get("/api/trips/:id", async (req, res) => {
   try {
-    pool.connect(async (error, client, release) => {
-      const resp = await client.query(
-        `
+    const result = await db.query(
+      `
         SELECT * 
         FROM trips
         WHERE id = $1
       `,
-        [req.params.id]
-      );
-      const photoResponse = await client.query(
-        `
+      [req.params.id]
+    );
+    const photoResponse = await db.query(
+      `
         SELECT * 
         FROM photos
         WHERE trip_id = $1
       `,
-        [req.params.id]
-      );
+      [req.params.id]
+    );
 
-      const trip = resp.rows[0];
-      const photos = photoResponse.rows;
-      // console.log("trip:", trip);
-      // console.log("photos:", photos);
+    const trip = result.rows[0];
+    const photos = photoResponse.rows;
 
-      res.json({ ...trip, photos });
-    });
+    res.json({ ...trip, photos });
   } catch (error) {
-    console.log(error);
     res.send({ error: error });
   }
 });
 
 // ----- PHOTOS ----- //
 
-// form to upload more photos
-App.get("/api/photos/new", (req, res) => {
-  res.send("form to upload more photos");
-});
-
 // create new photos in db
-App.post("/api/photos", (req, res) => {
-  // res.send("create new photos in db");
-  console.log(req.body);
-
+App.post("/api/photos", async (req, res) => {
   try {
-    pool.connect(async (error, client, release) => {
-      const queryParams = Object.values(req.body);
+    const queryParams = Object.values(req.body);
 
-      let resp = await client.query(
-        `
+    const result = await db.query(
+      `
         INSERT INTO photos
         (photo_text, date, lat, long, photo_url, trip_id) 
         VALUES
         ($1, $2, $3, $4, $5, $6)
         RETURNING *;
         `,
-        queryParams
-      );
-      console.log("response:", resp.rows[0]);
-      res.json(resp.rows[0]);
-    });
+      [queryParams]
+    );
+    console.log(result.rows);
+    res.json(result.rows[0]);
   } catch (error) {
-    console.log(error);
+    res.send({ error: error });
   }
 });
 
