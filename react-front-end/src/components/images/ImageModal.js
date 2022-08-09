6246449f221d204c8feb658ee6695db1314c924a
Jeff Bush <jeffbushdesign@gmail.com>
2022-08-09 12:27:38 -0400

    Add static text and forms to Photo Detail modal

--- react-front-end/src/components/images/ImageModal.js
+++ react-front-end/src/components/images/ImageModal.js
@@ -4,8 +4,14 @@ import { Modal } from 'react-responsive-modal';
 import { useState } from 'react';
 import PhotoDetail from "./PhotoDetail";
 
-import "./ImageModal.css";
+// React Bootstrap
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
+
+
+import classes from "./ImageModal.module.css";
+import "./ImageModal.css";
 
 function ImageModal({ onClose, open, children, selectedImg }) {
 
@@ -18,13 +24,48 @@ function ImageModal({ onClose, open, children, selectedImg }) {
       center
     >
       <PhotoDetail selectedImg={selectedImg} />
-      <p>• Example of an existing blog post</p>
-      <p>• Another blog post / note</p>
-      <p>• An input field where you can write another blog post</p>
-      <p>• A Post Button</p>
+      <div className={classes.sidebar}>
 
 
-    </Modal>
+
+
+        <p>A: Form shown if photo_text is currently empty. A textarea form is shown to allow the user to write a description</p>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Control as="textarea" rows={5} placeholder="Tell us about this photo" />
+          </Form.Group>
+          <Button variant="warning" type="submit" className="m-2">
+            Post
+          </Button>
+        </Form>
+
+        <p>------------------------------------------</p>
+        <p>B: Form shown if there IS existing photo_text. The photo_text is displayed along with an edit button.</p>
+        <Form>
+          <p>Scuba Junction was a really cool scuba diving school in Ko Tao. We stayed here for 3 days learning to scuba dive. We went on 5 dives.</p>
+          <Button variant="success" type="submit" className="m-2">
+            Edit Post
+          </Button>
+        </Form>
+
+
+        <p>------------------------------------------</p>
+        <p>C: Form shown if the user clicks the Edit Post button. They are shown the text area form with the text pre-populated with the existing photo_text. There is a Cancel button and a Done button</p>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Control as="textarea" rows={5} placeholder="Scuba Junction was a really cool scuba diving school in Ko Tao. We stayed here for 3 days learning to scuba dive. We went on 5 dives." />
+          </Form.Group>
+          <Button variant="secondary" type="submit" className="m-2">
+            Cancel
+          </Button>
+          <Button variant="primary" type="submit" className="m-2">
+            Done
+          </Button>
+        </Form>
+      </div>
+
+
+    </Modal >
   );
 }
 
