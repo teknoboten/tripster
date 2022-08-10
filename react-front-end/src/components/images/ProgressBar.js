0f885dcdc974f4f81b8e72deec9617f234e2ad58
Jeff Bush <jeffbushdesign@gmail.com>
2022-08-10 00:09:51 -0400

    Get latitude data from uploaded photo

--- react-front-end/src/components/images/ProgressBar.js
+++ react-front-end/src/components/images/ProgressBar.js
@@ -5,6 +5,9 @@ import { motion } from "framer-motion";
 import classes from "./ProgressBar.module.css";
 import axios from "axios";
 
+// EXIF to get image long and lat coordinates
+import EXIF from 'exif-js';
+
 const ProgressBar = ({ file, setFile, setStoredUrl, trip, setTrip }) => {
   const { url, progress } = useStorage(file);
   console.log(progress);
@@ -12,36 +15,53 @@ const ProgressBar = ({ file, setFile, setStoredUrl, trip, setTrip }) => {
   const params = useParams();
   const trip_id = params.tripId;
 
-  async function updateDb(url, trip_id) {
+  async function updateDb(url, trip_id, file) {
     console.log(trip_id);
 
-    const newImage = {
-      photo_text: "Hello world!",
-      date: "2018-02-18T08:01:00.000Z",
-      lat: 11.2793497,
-      long: 43.8274546,
-      photo_url: url,
-      trip_id: trip_id,
-    };
+    let latitude = 0;
+    console.log('Before');
+    await EXIF.getData(file, async function () {
+      var exifData = EXIF.pretty(this);
+      if (exifData) {
+        const a = EXIF.getTag(this, 'GPSLatitude');
+        const firstNum = a[0].valueOf();
+        const secondNum = a[1].valueOf();
+        const thirdNum = a[2].valueOf();
+        latitude = firstNum + secondNum / 60 + thirdNum / 3600;
+        console.log('Latitude in decimals:', latitude);
+      } else {
+        console.log("No EXIF data found in image '" + file.name + "'.");
+      }
+      const newImage = {
+        photo_text: "Hello world!",
+        date: "2018-02-18T08:01:00.000Z",
+        lat: latitude,
+        long: 43.8274546,
+        photo_url: url,
+        trip_id: trip_id,
+      };
+      console.log('newImage:', newImage);
 
-    const response = await fetch("/api/photos", {
-      method: "POST",
-      body: JSON.stringify(newImage),
-      headers: { "Content-Type": "application/json" },
-    });
+      const response = await fetch("/api/photos", {
+        method: "POST",
+        body: JSON.stringify(newImage),
+        headers: { "Content-Type": "application/json" },
+      });
 
-    axios.get(`/api/trips/${trip_id}`).then((result) => {
-      setTrip(result.data);
+      axios.get(`/api/trips/${trip_id}`).then((result) => {
+        setTrip(result.data);
+      });
+      console.log(trip.photos);
+      console.log("response:", response);
     });
-    console.log(trip.photos);
-    console.log("response:", response);
+    console.log('After');
   }
 
   useEffect(() => {
     if (url) {
       setFile(null);
       setStoredUrl(url);
-      updateDb(url, trip_id);
+      updateDb(url, trip_id, file);
     }
   }, [url, setFile]);
 
--- react-front-end/src/components/images/UploadImageForm.js
+++ react-front-end/src/components/images/UploadImageForm.js
@@ -3,6 +3,8 @@ import ProgressBar from "./ProgressBar";
 import classes from "./UploadImageForm.module.css";
 import { useHistory } from "react-router-dom";
 
+
+
 const UploadImageForm = ({ trip_id, trip, setTrip }) => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
@@ -24,13 +26,40 @@ const UploadImageForm = ({ trip_id, trip, setTrip }) => {
     let selected = e.target.files[0];
     // console.log(selected);
 
+    //------ Existing validation ------
     if (selected && types.includes(selected.type)) {
       setFile(selected);
       setError("");
+      // EXIF.getData(selected, function () {
+      //   var exifData = EXIF.pretty(this);
+      //   if (exifData) {
+      //     // console.log(exifData);
+
+      //     function calculateGPSCoordinates = (arr) => {
+
+      //     }
+      //     const a = EXIF.getTag(this, 'GPSLatitude');
+      //     const firstNum = a[0].valueOf();
+      //     const secondNum = a[1].valueOf();
+      //     const thirdNum = a[2].valueOf();
+      //     const latitude = firstNum + secondNum / 60 + thirdNum / 3600;
+      //     console.log('Latitude in decimals:', latitude);
+      //   } else {
+      //     console.log("No EXIF data found in image '" + selected.name + "'.");
+      //   }
+      // });
     } else {
       setFile(null);
       setError(`please select an image file (png or jpg)`);
     }
+
+
+
+
+
+
+
+
   };
 
   return (
