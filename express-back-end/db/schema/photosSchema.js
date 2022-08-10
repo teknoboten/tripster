3d36b72db1dbc39a3dbe0ae7a8a1d260b40fc4b7
Jeff Bush <41748727+jeffbushdesign@users.noreply.github.com>
2022-08-10 14:50:08 -0400

    Make map points read from database (#34)

--- express-back-end/db/schema/photosSchema.js
+++ express-back-end/db/schema/photosSchema.js
@@ -5,8 +5,8 @@ CREATE TABLE photos (
   id SERIAL PRIMARY KEY NOT NULL,
   photo_text TEXT,
   date TIMESTAMP,
-  lat DECIMAL (11,8) NOT NULL,
   long DECIMAL(11,8) NOT NULL,
+  lat DECIMAL (10,8) NOT NULL,
   photo_url VARCHAR(255) NOT NULL,
   trip_id INTEGER REFERENCES trips(id) ON DELETE CASCADE
 );`;
--- express-back-end/db/seeds/photos.sql
+++ express-back-end/db/seeds/photos.sql
@@ -1,18 +1,18 @@
 INSERT INTO photos (photo_text, date, coordinates, photo_url, trip_id ) 
-VALUES ('welcome to Italia!!', '2018-02-12T08:01:00.000Z', ARRAY [9.678560, 44.137000], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_0519.JPG?alt=media&token=79ad7b68-d9e8-42dc-89cc-72a3f5dae374', 1 );
+VALUES ('welcome to Italia!!', '2018-02-12T08:01:00.000Z', ARRAY [44.137000, 9.678560], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_0519.JPG?alt=media&token=79ad7b68-d9e8-42dc-89cc-72a3f5dae374', 1 );
 
 INSERT INTO photos (photo_text, date, coordinates, photo_url, trip_id ) 
-VALUES ('Wow', '2018-02-14T08:01:00.000Z', ARRAY [11.2793497, 43.8274546], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_0503.JPG?alt=media&token=bba8f2b2-719e-4857-ad5d-58a3a2d496f8', 1 );
+VALUES ('Wow', '2018-02-14T08:01:00.000Z', ARRAY [43.8274546, 11.2793497], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_0503.JPG?alt=media&token=bba8f2b2-719e-4857-ad5d-58a3a2d496f8', 1 );
 
 INSERT INTO photos (photo_text, date, coordinates, photo_url, trip_id ) 
-VALUES ('will i ever get tired of pizza?', '2018-02-18T08:01:00.000Z', ARRAY [11.2793497, 43.8274546], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/big-dodzy-uROkgCZO1H0-unsplash.jpg?alt=media&token=87def45d-006a-42d8-9591-527ab38dff12', 1 );
+VALUES ('will i ever get tired of pizza?', '2018-02-18T08:01:00.000Z', ARRAY [43.8274546, 11.2793497], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/big-dodzy-uROkgCZO1H0-unsplash.jpg?alt=media&token=87def45d-006a-42d8-9591-527ab38dff12', 1 );
 
 INSERT INTO photos (photo_text, date, coordinates, photo_url, trip_id ) 
-VALUES ('Perfect day for flying', '2015-11-10T15:15:00.00Z', ARRAY [10.94911667, 108.2080472], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_2127.jpeg?alt=media&token=b3bd8a09-7d1a-4ad1-89dc-ad6f848ff057', 2 );
+VALUES ('Perfect day for flying', '2015-11-10T15:15:00.00Z', ARRAY [108.2080472, 10.94911667], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_2127.jpeg?alt=media&token=b3bd8a09-7d1a-4ad1-89dc-ad6f848ff057', 2 );
 
 INSERT INTO photos (photo_text, date, coordinates, photo_url, trip_id ) 
-VALUES ('Perfect day for flying', '2015-12-07T11:05:00.00Z', ARRAY [19.99201667, 99.86023056], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_5653.jpeg?alt=media&token=e61bc151-3d66-4cfc-b41a-27634cba847e', 2 );
+VALUES ('Perfect day for flying', '2015-12-07T11:05:00.00Z', ARRAY [99.86023056, 19.99201667], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_5653.jpeg?alt=media&token=e61bc151-3d66-4cfc-b41a-27634cba847e', 2 );
 
 INSERT INTO photos (photo_text, date, coordinates, photo_url, trip_id ) 
-VALUES ('Happy hour!', '2015-09-20T10:20:00.00Z', ARRAY [10.09605556, 99.82796389], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_0080.jpeg?alt=media&token=487e1250-d4d8-47b6-9a56-9908bf9013fb', 2 );
+VALUES ('Happy hour!', '2015-09-20T10:20:00.00Z', ARRAY [99.82796389, 10.09605556], 'https://firebasestorage.googleapis.com/v0/b/trpstr-424aa.appspot.com/o/IMG_0080.jpeg?alt=media&token=487e1250-d4d8-47b6-9a56-9908bf9013fb', 2 );
 
