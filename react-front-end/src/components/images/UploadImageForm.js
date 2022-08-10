11125c499b230b8303ac6d2f313ade79bd84967b
serra <serra@modemgirl.ca>
2022-08-10 13:01:25 -0700

    stash commit. upload logic currently broken.

--- react-front-end/src/components/images/UploadImageForm.js
+++ react-front-end/src/components/images/UploadImageForm.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import ProgressBar from "./ProgressBar";
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import classes from "./UploadImageForm.module.css";
+import useStorage from "../../hooks/useStorage";
 import { useHistory } from "react-router-dom";
 import useInput from "../../hooks/useInput";
+import axios from "axios";
 
 import LocationInputField from './LocationInputField';
 import SaySomethingInput from "./SaySomethingInput";
@@ -16,22 +18,43 @@ const UploadImageForm = ({ trip, setTrip }) => {
   const [storedUrl, setStoredUrl] = useState("");  
   const [coordinates, setCoordinates] = useState(null);
   const [photoText, setPhotoText] = useState("");
- 
 
   const history = useHistory();
   const location = useInput("");
 
+  // async function updateDb(url, trip_id, coordinates) {
+    
+  //   const newImage = {
+  //     photo_text: "Hello world!",
+  //     date: "2018-02-18T08:01:00.000Z",
+  //     photo_url: url,
+  //     trip_id: trip_id,
+  //     coordinates: coordinates
+  //   };
+
+  //   console.log("creating a new image in the db:", newImage);
+
+  //   const response = await fetch("/api/photos", {
+  //     method: "POST",
+  //     body: JSON.stringify(newImage),
+  //     headers: { "Content-Type": "application/json" },
+  //   });
+
+  //   axios.get(`/api/trips/${trip_id}`).then((result) => {
+  //     setTrip(result.data);
+  //   });
+  //   console.log(trip.photos);
+  //   console.log("response:", response);
+  // }
 
   const types = ["image/png", "image/jpeg"];
 
   useEffect(() => {
-    //post to the DB
     if (storedUrl !== "") {
-      //save storedURL to the db
-      // .then()
       history.push(`/trips/${trip.id}`);
     }
   }, [storedUrl]);
+
 
   const changedImgHandler = (e) => {
     let selected = e.target.files[0];
@@ -53,10 +76,6 @@ const UploadImageForm = ({ trip, setTrip }) => {
         <span>Select an image to add</span>
       </label>
 
-      {file && (
-        <ImagePreview img={file.value}/>
-      )}
-
       
       {/* <LocationInputField placeholder="Location"
         {...location}
@@ -71,16 +90,7 @@ const UploadImageForm = ({ trip, setTrip }) => {
       <div className={classes.output}>
         {error && <div className={classes.error}>{error} </div>}
         {file && <div>{file.name}</div>}
-        {file && (
-          <ProgressBar
-            file={file}
-            setFile={setFile}
-            setStoredUrl={setStoredUrl}
-            trip={trip}
-            setTrip={setTrip}
-            coordinates={coordinates}
-          />
-        )}
+        {file && <ProgressBar file={file} setFile={setFile} />}
 
 
         
