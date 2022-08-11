5f06c156e98eb172566cf8d4deef2516dd34e917
amyfranses <amyfranses@gmail.com>
2022-08-11 15:49:53 +0000

    update styling on tripDetailPage

--- react-front-end/src/components/layout/MainNav.js
+++ react-front-end/src/components/layout/MainNav.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import classes from './MainNav.module.css';
+import classes from "./MainNav.module.css";
 
 function MainNav() {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>Tripster</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>All Trips</Link>
-          </li>
-          <li>
-            <Link to='/new-trip'>Add New Trip</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className={classes.navContainer}>
+      <header className={classes.header}>
+        <div className={classes.logo}>Tripster</div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Trips</Link>
+            </li>
+            <li>
+              <Link to="/new-trip">Add New Trip</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
 
