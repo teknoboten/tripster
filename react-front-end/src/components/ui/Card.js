b0d2c25a142efdc57877540f5b214ef28608a40a
amyfranses <75110673+amyfranses@users.noreply.github.com>
2022-08-11 15:49:16 +0100

    Css/styling updates (#38)
    * update styling
    
    * increase map component width

--- react-front-end/src/components/ui/Card.js
+++ react-front-end/src/components/ui/Card.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import classes from './Card.module.css';
+import React from "react";
+import classes from "./Card.module.css";
 
 function Card(props) {
   return <div className={classes.card}>{props.children}</div>;
